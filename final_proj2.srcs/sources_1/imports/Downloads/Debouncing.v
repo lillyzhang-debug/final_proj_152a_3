e783aef1a96aee100cb75ecd7e71d81f0bda4bfa
juliac888 <119269739+juliac888@users.noreply.github.com>
2025-12-01 16:11:27 -0800

    deboucing should not reset button detection

--- final_proj2.srcs/sources_1/imports/Downloads/Debouncing.v
+++ final_proj2.srcs/sources_1/imports/Downloads/Debouncing.v
@@ -21,44 +21,30 @@ reg [1:0] sync_sw15, sync_sw14, sync_sw13; //not sure how many switches
 reg [1:0] sync_btnS, sync_btnA, sync_btnB, sync_btnC, sync_btnD;
 
 always @(posedge clk) begin
-
-    if (start == 1'b1) begin
-        sync_sw15 <= 2'b00;
-        sync_sw14 <= 2'b00;
-		sync_sw13 <= 2'b00;
-	
-        sync_btnS <= 2'b00;
-        sync_btnA <= 2'b00;
-		sync_btnB <= 2'b00;
-		sync_btnC <= 2'b00;
-		sync_btnD <= 2'b00;
-    end else begin //two flip flops 
-
-        sync_sw15[0] <= sw[15];
-        sync_sw15[1] <= sync_sw0[0];
+    sync_sw15[0] <= sw[15];
+    sync_sw15[1] <= sync_sw0[0];
     
-        sync_sw14[0] <= sw[14];
-        sync_sw14[1] <= sync_sw14[0];
+    sync_sw14[0] <= sw[14];
+    sync_sw14[1] <= sync_sw14[0];
 
-		sync_sw13[0] <= sw[13];
-		sync_sw13[1] <= sync_sw13[0];
+	sync_sw13[0] <= sw[13];
+	sync_sw13[1] <= sync_sw13[0];
         
-        //buttons
-		sync_btnS[0] <= btnS;
-		sync_btnS[1] <= sync_btnS[0];
+    //buttons
+	sync_btnS[0] <= btnS;
+	sync_btnS[1] <= sync_btnS[0];
         
-        sync_btnA[0] <= btnA;
-        sync_btnA[1] <= sync_btnA[0];
+    sync_btnA[0] <= btnA;
+    sync_btnA[1] <= sync_btnA[0];
 
-		sync_btnB[0] <= btnB;
-        sync_btnB[1] <= sync_btnB[0];
+	sync_btnB[0] <= btnB;
+    sync_btnB[1] <= sync_btnB[0];
 
-		sync_btnC[0] <= btnC;
-        sync_btnC[1] <= sync_btnC[0];
+	sync_btnC[0] <= btnC;
+    sync_btnC[1] <= sync_btnC[0];
 
-		sync_btnD[0] <= btnD;
-        sync_btnD[1] <= sync_btnD[0];
-	end
+	sync_btnD[0] <= btnD;
+    sync_btnD[1] <= sync_btnD[0];
 end   
 
 //debouncing make sure it has been 'on' for two samples (2ms)
@@ -69,52 +55,30 @@ reg stable_sw15, stable_sw14, stable_sw13;
 reg stable_btnS, stable_btnA, stable_btnB, stable_btnC, stable_btnD;
 
 always @(posedge clk) begin
-
-    if (start == 1'b1) begin
-        deb_sw15 <= 2'b00;
-        deb_sw14 <= 2'b00;
-		deb_sw13 <= 2'b00;
-        deb_btnS <= 2'b00;
-        deb_btnA <= 2'b00;
-		deb_btnB <= 2'b00;
-		deb_btnC <= 2'b00;
-		deb_btnD <= 2'b00;
-		
-        stable_sw15 <= 1'b0;
-        stable_sw14 <= 1'b0;
-		stable_sw13 <= 1'b0;
-        stable_btnS <= 1'b0;
-        stable_btnA <= 1'b0;
-		stable_btnB <= 1'b0;
-		stable_btnC <= 1'b0;
-		stable_btnD <= 1'b0;
-
-    end else begin
-
-        deb_sw15[0] <= deb_sw15[1];
-        deb_sw15[1] <= deb_sw15[0];
+    deb_sw15[0] <= deb_sw15[1];
+    deb_sw15[1] <= deb_sw15[0];
     
-        deb_sw14[0] <= deb_sw14[1];
-        deb_sw14[1] <= deb_sw14[0];
+    deb_sw14[0] <= deb_sw14[1];
+    deb_sw14[1] <= deb_sw14[0];
 
-		deb_sw13[0] <= deb_sw13[1];
-		deb_sw13[1] <= deb_sw13[0];
+	deb_sw13[0] <= deb_sw13[1];
+	deb_sw13[1] <= deb_sw13[0];
     
-		deb_btnS[0] <= deb_btnS[1];
-		deb_btnS[1] <= deb_btnS[0];
+	deb_btnS[0] <= deb_btnS[1];
+	deb_btnS[1] <= deb_btnS[0];
     
-        deb_btnA[0] <= deb_btnA[1];
-        deb_btnA[1] <= deb_btnA[0];
+    deb_btnA[0] <= deb_btnA[1];
+    deb_btnA[1] <= deb_btnA[0];
 
-		deb_btnB[0] <= deb_btnB[1];
-        deb_btnB[1] <= deb_btnB[0];
+	deb_btnB[0] <= deb_btnB[1];
+    deb_btnB[1] <= deb_btnB[0];
 
-		deb_btnC[0] <= deb_btnC[1];
-        deb_btnC[1] <= deb_btnC[0];
+	deb_btnC[0] <= deb_btnC[1];
+    deb_btnC[1] <= deb_btnC[0];
 
-		deb_btnD[0] <= deb_btnD[1];
-        deb_btnD[1] <= deb_btnD[0];
-	end
+	deb_btnD[0] <= deb_btnD[1];
+    deb_btnD[1] <= deb_btnD[0];
+	
 	//if both are 11 switch is on, if both 00 switch is off, if diff current state
 	stable_sw15 <= (deb_sw15 == 2'b11) ? 1'b1 : (deb_sw15 == 2'b00) ? 1'b0 : stable_sw15;
 	stable_sw14 <= (deb_sw14 == 2'b11) ? 1'b1 : (deb_sw14 == 2'b00) ? 1'b0 : stable_sw14;
@@ -137,45 +101,24 @@ wire btnD_pressed_edge = stable_btnD && !prev_btnD; // D
 
 //assign button values
 always @(posedge clk) begin
+    prev_btnS <= stable_btnS;
+    prev_btnA <= stable_btnA;
+	prev_btnB <= stable_btnB;
+	prev_btnC <= stable_btnC;
+	prev_btnD <= stable_btnD;
 
-    if (start == 1'b1) begin
-       	prev_btnS <= 1'b0;
-        prev_btnA <= 1'b0;
-		prev_btnB <= 1'b0;
-		prev_btnC <= 1'b0;
-		prev_btnD <= 1'b0;
-        start <= 1'b0;
-        a <= 1'b0;
-		b <= 1'b0;
-		c <= 1'b0;
-		d <= 1'b0;
-
-    end else begin
-       	prev_btnS <= stable_btnS;
-        prev_btnA <= stable_btnA;
-		prev_btnB <= stable_btnB;
-		prev_btnC <= stable_btnC;
-		prev_btnD <= stable_btnD;
-
-		start <= btnS_pressed_edge;
-		a <= btnA_pressed_edge;
-		b <= btnB_pressed_edge;
-		c <= btnC_pressed_edge;
-		d <= btnD_pressed_edge;
-    end
+	start <= btnS_pressed_edge;
+	a <= btnA_pressed_edge;
+	b <= btnB_pressed_edge;
+	c <= btnC_pressed_edge;
+	d <= btnD_pressed_edge;
 end
 
 //assign switch values
 always @(posedge clk) begin
-    if (start == 1'b1) begin
-        sw15 <= 1'b0;
-        sw14 <= 1'b0; 
-		sw13 <= 1'b0; 
-    end else begin
-        sw15 <= stable_sw15;
-        sw14 <= stable_sw14;
-		sw13 <= stable_sw13;
-    end
+    sw15 <= stable_sw15;
+    sw14 <= stable_sw14;
+	sw13 <= stable_sw13;
 end 
 
 endmodule
