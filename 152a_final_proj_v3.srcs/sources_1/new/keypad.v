2851255bae2134ca0102f28f8a1a70496ebfcab4
juliac888 <119269739+juliac888@users.noreply.github.com>
2025-12-02 17:48:56 -0800

    Update keypad.v, added clock

--- 152a_final_proj_v3.srcs/sources_1/new/keypad.v
+++ 152a_final_proj_v3.srcs/sources_1/new/keypad.v
@@ -1,4 +1,5 @@
 module keypad(
+    input clk,
     input  wire [3:0] row,   // JB[7:4]
     output wire       col4,  // JB[3]
     output wire       btnA_raw,
@@ -7,11 +8,12 @@ module keypad(
     output wire       btnD_raw
 );
 
-assign col4 = 1'b0;  // col4 = A/B/C/D
-
-assign btnA_raw = (row == 4'b1110);  // A
-assign btnB_raw = (row == 4'b1101);  // B
-assign btnC_raw = (row == 4'b1011);  // C
-assign btnD_raw = (row == 4'b0111);  // D
-
+always @(posedge clk) begin
+    col4 <= 1'b0;  // col4 = A/B/C/D
+    
+    btnA_raw <= (row == 4'b1110);  // A
+    btnB_raw <= (row == 4'b1101);  // B
+    btnC_raw <= (row == 4'b1011);  // C
+    btnD_raw <= (row == 4'b0111);  // D
+end
 endmodule
